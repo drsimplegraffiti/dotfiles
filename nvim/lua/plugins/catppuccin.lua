af49bcd31d0f961ee7f149a6abccd7c4ffefc7ec
drsimplegraffiti <abayomiogunnusi@gmail.com>
2025-02-19 23:06:32 +0100

    added config

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,33 @@
+-- return {
+--   "catppuccin/nvim",
+--   lazy = false,
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme "catppuccin" 
+--   end
+-- }
+
+-- return {
+--   "sainnhe/everforest",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme "everforest"
+--   end
+-- }
+
+
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin" 
-  end
+    "tiagovla/tokyodark.nvim",
+    opts = {
+        -- Custom options (optional)
+        transparent_background = true,  -- Set true for transparency
+        gamma = 1.0,  -- Adjust brightness (default: 1.0)
+        italic_comments = true,  -- Enable italic comments
+    },
+    config = function(_, opts)
+        require("tokyodark").setup(opts)  -- Optional setup call
+        vim.cmd [[colorscheme tokyodark]]
+    end,
 }
+
